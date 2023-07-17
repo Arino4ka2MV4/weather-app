e701ee49c21ab1bbfb93f45603862e73e302b0c9
Arino4ka2MV4 <113789360+Arino4ka2MV4@users.noreply.github.com>
2023-07-17 21:54:31 +0300

    Add files via upload

---
+++ consts.mjs
@@ -0,0 +1,35 @@
+export const cityNames = [
+    {
+        eng: 'Saint-Petersburg',
+        ru: 'Санкт-Петербург'
+    },
+    {
+        eng: 'London',
+        ru: 'Лондон'
+    },
+    {
+        eng: 'Vienna',
+        ru: 'Вена'
+    },
+    {
+        eng: 'Berlin',
+        ru: 'Берлин'
+    },
+    {
+        eng: 'Tokyo',
+        ru: 'Токио'
+    },
+    {
+        eng: 'New-York',
+        ru: 'Нью-Йорк'
+    },
+    {
+        eng: 'Moscow',
+        ru: 'Москва'
+    }
+]
+
+
+export const params = {
+    apiKey: 'ee5250567821041bea9f7f4fc43929ed'
+}
